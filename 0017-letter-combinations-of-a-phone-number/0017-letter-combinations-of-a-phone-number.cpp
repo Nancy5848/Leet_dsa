531a6faa085cf3a7c85b690d1f8e8715fe6405c2
Nancy <149788546+Nancy5848@users.noreply.github.com>
2026-07-28 21:41:13 +0530

    Time: 0 ms (100%), Space: 9.4 MB (31.78%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> ans;
+    vector<string> mp = {
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"
+    };
+
+    void solve(string digits, int index, string temp) {
+        if (index == digits.size()) {
+            ans.push_back(temp);
+            return;
+        }
+
+        string letters = mp[digits[index] - '0'];
+
+        for (char ch : letters) {
+            solve(digits, index + 1, temp + ch);
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty())
+            return {};
+
+        solve(digits, 0, "");
+        return ans;
+    }
+};
