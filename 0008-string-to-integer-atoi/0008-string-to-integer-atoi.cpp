1c55629409bb7820a9b0688316cafd2c2be14f3a
Nancy <149788546+Nancy5848@users.noreply.github.com>
2026-07-28 21:38:20 +0530

    Time: 0 ms (100%), Space: 9.2 MB (21.27%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, n = s.size();
+        int sign = 1;
+        int result = 0;
+
+        // Skip leading spaces
+        while (i < n && s[i] == ' ')
+            i++;
+
+        // Check sign
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-')
+                sign = -1;
+            i++;
+        }
+
+        // Convert digits
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+
+            // Check overflow
+            if (result > INT_MAX / 10 ||
+                (result == INT_MAX / 10 && digit > 7)) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+
+        return result * sign;
+    }
+};
