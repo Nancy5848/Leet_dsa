055b66ccfc17d0806c888a74b3d29f13f0263580
Nancy <149788546+Nancy5848@users.noreply.github.com>
2026-08-16 18:40:15 +0530

    Time: 12 ms (12.18%), Space: 131.3 MB (69.29%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0, 0, 0};
+
+        // Count stones based on remainder when divided by 3
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        int zero = cnt[0];
+        int one = cnt[1];
+        int two = cnt[2];
+
+        // Case 1: Even number of stones divisible by 3
+        if (zero % 2 == 0) {
+            return one > 0 && two > 0;
+        }
+
+        // Case 2: Odd number of stones divisible by 3
+        return abs(one - two) > 2;
+    }
+};
