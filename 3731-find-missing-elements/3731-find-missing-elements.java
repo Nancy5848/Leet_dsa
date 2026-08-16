2b8bb00302bbab88a17c4d4ca3558533a892a62e
Nancy <149788546+Nancy5848@users.noreply.github.com>
2026-08-16 18:43:19 +0530

    Time: 6 ms (42.41%), Space: 47 MB (44.73%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        
+        Arrays.sort(nums);
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            
+            for (int j = nums[i] + 1; j < nums[i + 1]; j++) {
+                result.add(j);
+            }
+        }
+
+        return result;
+    }
+}
